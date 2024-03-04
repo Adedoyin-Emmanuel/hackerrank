aab28a53df95319a6641fa03402fcd6e94bbe4cd
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-03-04 06:16:18 +0100

    Fix: Removed uncessary codes

--- day-1/plus-minus.ts
+++ day-1/plus-minus.ts
@@ -1,5 +1,5 @@
 /**
-        Submissions	Leaderboard	Discussions	Editorial
+        
         Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 
         Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
