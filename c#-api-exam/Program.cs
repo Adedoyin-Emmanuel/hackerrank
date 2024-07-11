e4fd29e76291e4b54081523831797089edbbd977
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-07-11 08:46:51 +0100

    fix: skipped bin and node_modules from VCS

--- c#-api-exam/Program.cs
+++ c#-api-exam/Program.cs
@@ -1,2 +1,11 @@
 ﻿using System;
 
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
--- c#-exam/Program.cs
+++ c#-exam/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 
 class Program
 {
+
 
     public static void fizzBuzz(int n)
     {
@@ -28,92 +32,115 @@ class Program
     }
 
 
-    public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+    // public static Dictionary<string, int> AverageAgeForEachCompany(List<Employee> employees)
+    // {
+    //     var groupedByCompany = employees.GroupBy(employee => employee.Company);
+
+    //     Dictionary<string, int> avgAge = groupedByCompany.Select(group => new
+    //     {
+    //         Company = group.Key,
+    //         AverageAge = (int)group.Average(employee => employee.Age)
+    //     }).ToDictionary(kvp => kvp.Company, kvp => kvp.AverageAge);
+
+
+    //     return avgAge;
+
+    // }
+
+    // public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+    // {
+
+    //     var groupedByCompany = employees.GroupBy(employee => employee.Company);
+
+    //     Dictionary<string, int> count = groupedByCompany.Select(group => new
+    //     {
+    //         Company = group.Key,
+    //         Count = group.Count()
+    //     }).ToDictionary(kvp => kvp.Company, kvp => kvp.Count);
+
+    //     return count;
+    // }
+
+    // public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
+    // {
+
+    //     var groupedByCompany = employees.GroupBy(employee => employee.Company);
+    //     Dictionary<string, Employee> oldest = groupedByCompany.Select(group => new
+    //     {
+    //         Company = group.Key,
+    //         OldestEmployee = group.OrderByDescending(employee => employee.Age).FirstOrDefault()
+    //     }).ToDictionary(kvp => kvp.Company, kvp => kvp.OldestEmployee);
+
+
+    //     return oldest;
+    // }
+
+
+    public static async Task<int> getTotalGoals(string team, int year)
     {
-        var groupedByCompany = employees.GroupBy(employee => employee.Company);
+        HttpClient client = new HttpClient();
 
-        Dictionary<string, int> avgAge = groupedByCompany.Select(group => new
-        {
-            Company = group.Key,
-            AverageAge = (int)group.Average(employee => employee.Age)
-        }).ToDictionary(kvp => kvp.Company, kvp => kvp.AverageAge);
+        int totalGoals = 0;
+        int page = 1;
+        string teamUrl1 = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team1={team}&page={page}";
+        string teamUrl2 = $"https://jsonmock.hackerrank.com/api/football_matches?year={year}&team2={team}&page={page}";
 
 
-        return avgAge;
+        var json = await client.GetStringAsync(teamUrl1);
+
+        Console.WriteLine(json);
+
+        return totalGoals;
 
     }
 
-    public static Dictionary<string, int> CountOfEmployeesForEachCompany(List<Employee> employees)
+    // public static void Main()
+    // {
+    //     int countOfEmployees = int.Parse(Console.ReadLine());
+
+    //     var employees = new List<Employee>();
+
+    //     for (int i = 0; i < countOfEmployees; i++)
+    //     {
+    //         string str = Console.ReadLine();
+    //         string[] strArr = str.Split(' ');
+    //         employees.Add(new Employee
+    //         {
+    //             FirstName = strArr[0],
+    //             LastName = strArr[1],
+    //             Company = strArr[2],
+    //             Age = int.Parse(strArr[3])
+    //         });
+    //     }
+
+    //     foreach (var emp in AverageAgeForEachCompany(employees))
+    //     {
+    //         Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
+    //     }
+
+    //     foreach (var emp in CountOfEmployeesForEachCompany(employees))
+    //     {
+    //         Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
+    //     }
+
+    //     foreach (var emp in OldestAgeForEachCompany(employees))
+    //     {
+    //         Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
+    //     }
+    // }
+
+
+    public static void Main(string[] args)
     {
-
-        var groupedByCompany = employees.GroupBy(employee => employee.Company);
-
-        Dictionary<string, int> count = groupedByCompany.Select(group => new
-        {
-            Company = group.Key,
-            Count = group.Count()
-        }).ToDictionary(kvp => kvp.Company, kvp => kvp.Count);
-
-        return count;
-    }
-
-    public static Dictionary<string, Employee> OldestAgeForEachCompany(List<Employee> employees)
-    {
-
-        var groupedByCompany = employees.GroupBy(employee => employee.Company);
-        Dictionary<string, Employee> oldest = groupedByCompany.Select(group => new
-        {
-            Company = group.Key,
-            OldestEmployee = group.OrderByDescending(employee => employee.Age).FirstOrDefault()
-        }).ToDictionary(kvp => kvp.Company, kvp => kvp.OldestEmployee);
-
-        
-        return oldest;
-    }
-
-
-    public static void Main()
-    {
-        int countOfEmployees = int.Parse(Console.ReadLine());
-
-        var employees = new List<Employee>();
-
-        for (int i = 0; i < countOfEmployees; i++)
-        {
-            string str = Console.ReadLine();
-            string[] strArr = str.Split(' ');
-            employees.Add(new Employee
-            {
-                FirstName = strArr[0],
-                LastName = strArr[1],
-                Company = strArr[2],
-                Age = int.Parse(strArr[3])
-            });
-        }
-
-        foreach (var emp in AverageAgeForEachCompany(employees))
-        {
-            Console.WriteLine($"The average age for company {emp.Key} is {emp.Value}");
-        }
-
-        foreach (var emp in CountOfEmployeesForEachCompany(employees))
-        {
-            Console.WriteLine($"The count of employees for company {emp.Key} is {emp.Value}");
-        }
-
-        foreach (var emp in OldestAgeForEachCompany(employees))
-        {
-            Console.WriteLine($"The oldest employee of company {emp.Key} is {emp.Value.FirstName} {emp.Value.LastName} having age {emp.Value.Age}");
-        }
+        await getTotalGoals("Barcelona", 2021);
     }
 
 
-
-    public class Employee
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public string Company { get; set; }
-    }
+    // public class Employee
+    // {
+    //     public string FirstName { get; set; }
+    //     public string LastName { get; set; }
+    //     public int Age { get; set; }
+    //     public string Company { get; set; }
+    // }
 }
